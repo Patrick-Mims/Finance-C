be4505b62c90db251d9b9fa144fae3866ae5b67e
patrick <patrickkevinmims@gmail.com>
2025-04-21 21:13:32 -0700

    latest

--- src/thread.c
+++ src/thread.c
@@ -1,2 +1,13 @@
 #include "finance.h"
 
+void thread_factory(pthread_t *thread, const void *(*fn)(void *)) 
+{
+    void *result;
+
+    // if not successful then it equals a failure.
+    if (pthread_create(&thread, NULL, fn, NULL) != 0)
+        fprintf(stderr, "Error: pthread_create()");
+
+    pthread_join(thread, &result);
+    pthread_exit();
+}
