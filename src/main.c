6bcd3c8ea8d924ca900d8f091e4cffffbce3c7f5
patrick <patrickkevinmims@gmail.com>
2025-04-23 20:02:18 -0700

    updated main with data

--- src/main.c
+++ src/main.c
@@ -2,6 +2,19 @@
 #include "finsftp.h"
 #include "finssh.h"
 
+const char *funds[] = {
+    {"fbltx", 1241.28},
+    {"fctdx", 365560.63},
+    {"fgomx", 53475.74},
+    {"fifgx", 8255.25},
+    {"fiwgx", 57674.27},
+    {"fscjx", 9378.34},
+    {"fspwx", 13329},
+    {"fsrjx", 3205.11},
+    {"fusix", 112775.24},
+    {"fzolx", 5520.31},
+    {"fzomx", 3359.71}};
+
 int main()
 {
     puts("Finance 2.0 on the Dev branch 2");
