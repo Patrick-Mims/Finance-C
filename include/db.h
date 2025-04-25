0680ec8ddf8ae8da3c81c4002a2940760e1211b1
patrick <patrickkevinmims@gmail.com>
2025-04-24 19:19:03 -0700

    Added mysql binding

---
+++ include/db.h
@@ -0,0 +1,11 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <mysql/mysql.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void mysql_version();
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
+#include "database.h"
 #include "finance.h"
 #include "finsftp.h"
 #include "finssh.h"
+
+/*
+    Ticker and beginning balance
+    Task is to track these funds daily and update the 
+    Fidelity Table in the MySQL DB named Retirement.
+
+    use the mysql binding
+*/
 
 const char *funds[] = {
     {"fbltx", 1241.28},
@@ -18,12 +27,15 @@ const char *funds[] = {
 int main()
 {
     puts("Finance 2.0 on the Dev branch 2");
+    mysql_version();
 
     void *(*gt)() = g_trade;
+    void (*db)() = db;
 
-    pthread_t th_trade;
+    pthread_t th_db, th_trade;
 
     thread_factory(&th_trade, gt);
+    // thread_factory(&th_db, db);
 
     return 0;
 }
